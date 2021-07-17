865cc95f0c4217ebdc1ebb5e9c60b7212eec8868
Amirhossein <ah.rasoulian@gmail.com>
2021-07-17 13:13:37 +0430

    finished enter form script.

--- assets/script/enter_form.js
+++ assets/script/enter_form.js
@@ -1,6 +1,102 @@
 "use strict";
 
 
+document.getElementById("formid").addEventListener("submit", enterValidation)
+let users = [
+    {
+        "email": "ahr96@aut.ac.ir",
+        "pass": "a1234567"
+    },
+    {
+        "email": "ah.rasoulian@gmail.com",
+        "pass": "r1234567"
+    }
+]
+
+function removeModal(){
+    // find the modal and remove if it exists
+    let modal = document.querySelector('.modal')
+    if (modal) {
+      modal.remove()
+    }
+}
+
+function renderModal(status, message){
+    let status_message = ""
+    if (status){
+        status_message = "موفق"
+    }
+    else {
+        status_message = "ناموفق"
+    }
+
+    // create the background modal div
+    let modal = document.createElement('div')
+    modal.classList.add('modal')
+
+    // create the inner modal div with appended argument
+    let modal_content = document.createElement('div')
+    modal_content.classList.add('modal__content')
+
+    let exit_button = document.createElement('button')
+    exit_button.classList.add("modal_content--type-exit")
+    exit_button.addEventListener('click', removeModal)
+    exit_button.innerHTML = "X"
+    modal_content.appendChild(exit_button)
+
+    let header = document.createElement('h1')
+    header.classList.add('modal__content--type-header')
+    header.innerHTML = status_message
+    modal_content.appendChild(header)
+
+    let content = document.createElement('p')
+    content.classList.add('modal__content--type-content')
+    content.innerHTML = message
+    modal_content.appendChild(content)
+
+    // render the modal with child on DOM
+    modal.appendChild(modal_content)
+    document.body.appendChild(modal)
+
+    modal.addEventListener('click', event => {
+        if (event.target.className === 'modal') {
+          removeModal()
+        }
+    })
+}
+
+function enterValidation(event){
+    event.preventDefault()
+    let message = ""
+    let logged_in = false
+    
+    if(formValidation()){
+        let password = document.form.password.value;
+        let email = document.form.email.value;
+
+        for(let i = 0; i < users.length; i++){
+            if(users[i].email == email){
+                if(users[i].pass == password){
+                    logged_in = true
+                }
+                break
+            }
+        }
+
+        if (logged_in){
+            message = "خوش آمدید"
+        }
+        else {
+            message = "نام کاربری یا رمز عبور معتبر نیست"
+        }
+    }
+    else{
+        message = "نام کاربری یا رمز عبور اشتباه است! به خطاهای فرم دقت کنید."
+    }
+
+    renderModal(logged_in, message)
+}
+
 function formValidation(){
     let password = document.form.password.value;
     let email = document.form.email.value;
@@ -40,22 +136,13 @@ function requiredInput(input){
 }
 
 function emailFormat(email){
-    var atposition= email.indexOf("@");  
-    var dotposition=email.lastIndexOf(".");  
-    if (atposition<1 || dotposition<atposition+2 || dotposition+2>=email.length){ 
-        return false;
-    }
-    return true;
+    const pattern = /\S+@\S+\.\S+/
+    return pattern.test(email)
 }
 
 function passwordFormat(password){
-
-    console.log(password)
-    console.log(typeof(password))
     let length = password.length
-    const pattern1 = /[a-zA-Z]/
-    const pattern2 = /[0-9]/
-    if(length<8 || (!pattern1.test(password)) || (!pattern2.test(password)) ){
+    if(length<8 || (!containLetter(password)) || (!containNumber(password)) ){
         return false
     }
     return true;
@@ -82,11 +169,10 @@ function maxLength(input, max){
 }
 
 
-document.querySelector("input#email").addEventListener("input", emailValidation)
-document.querySelector("input#email").addEventListener("focusout", emailfocusout)
+document.getElementById("email").addEventListener("input", emailValidation)
+document.getElementById("email").addEventListener("focusout", focusout)
 
 function emailValidation(){
-    console.log('hello')
     let email = document.form.email.value;
     let error_messaegs = []
     let there_is_error = false
@@ -124,15 +210,14 @@ function emailValidation(){
     return false;
 }
 
-function emailfocusout(){
-    document.querySelector("#email").style.border = "none";
+function focusout(event){
+    event.target.style.border = "none";
 }
 
-document.querySelector("input#pass").addEventListener("input", emailValidation)
-document.querySelector("input#pass").addEventListener("focusout", passfocusout)
+document.getElementById("pass").addEventListener("input", passValidation)
+document.getElementById("pass").addEventListener("focusout", focusout)
 
-function emailValidation(){
-    console.log('hello')
+function passValidation(){
     let password = document.form.password.value;
     let error_messaegs = []
     let there_is_error = false
@@ -142,10 +227,10 @@ function emailValidation(){
         if(password.length < 8){
             error_messaegs.push("- رمز باید حداقل ۸ کاراکتر باشد")
         }
-        if(containLetter(password)){
+        if(!containLetter(password)){
             error_messaegs.push("- رمز باید شامل حرف باشد")
         }
-        if(containNumber(password)){
+        if(!containNumber(password)){
             error_messaegs.push("- رمز باید شامل عدد باشد")
         }
     }
@@ -173,8 +258,4 @@ function emailValidation(){
         div.innerHTML += "<p>" + error_messaegs[i] + "</p>"
     }
     return false;
-}
-
-function passfocusout(){
-    document.querySelector("#pass").style.border = "none";
 }
